e07c0a75ef6be7ce94b088b69c2c6d818c5004f4
Vinicius Koga <viniciuskoga_m@hotmail.com>
2022-12-06 23:01:14 -0300

    Searching text mod

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,17 @@ document.querySelector('.SearchGifBtn').addEventListener('click', checkInput);
 
 async function searchGif(e) {
     let input = document.querySelector('.searchGif');
-    if(input.value != '' && (e.key === 'Enter' || e.type === 'click')){
+    if (input.value != '' && (e.key === 'Enter' || e.type === 'click')) {
         let req = await fetch(`https://tenor.googleapis.com/v2/search?q=${input.value}&key=AIzaSyD0Gtagyi2BUKkJ-pwVD8irjgHq3kABbcQ&limit=8`)
         let reqJson = await req.json();
 
         document.querySelector('.gifs').innerHTML = `<p class="text-center">Searching...</p>`;
-        
+
         showGifs(reqJson);
 
         function showGifs(list) {
             let html = '';
-            for(let i=0; i<list.results.length; i++){
+            for (let i = 0; i < list.results.length; i++) {
                 html += `
                 <div class="col-md-4 d-flex justify-content-center align-items-center">                        
                     <img class="img-thumbnail" src="${list.results[i].media_formats.tinygif.url}" />
@@ -29,10 +29,10 @@ async function searchGif(e) {
 
 function checkInput(e) {
     let input = document.querySelector('.searchGif');
-    if(input.value == '' && (e.key === 'Enter' || e.type === 'click')){
-        document.querySelector('.gifs').innerHTML = '<p class="text-center">Acho que você precisa escrever algo :)</p>';
+    if (input.value == '' && (e.key === 'Enter' || e.type === 'click')) {
+        document.querySelector('.gifs').innerHTML = '<p class="text-center">Digite algo! :)</p>';
     }
-    if(input.value != ''){
+    if (input.value != '') {
         document.querySelector('.gifs').innerHTML = '';
     }
 }
